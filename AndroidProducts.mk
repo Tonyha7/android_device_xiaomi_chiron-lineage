c8a43ea2bb7c96f67aca21889cbc413a0ec91c57
th7 <69128819+Tonyha7@users.noreply.github.com>
2023-03-17 10:00:28 +0000

    chiron: Time 4 tequila

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chiron.mk
+    $(LOCAL_DIR)/tequila_chiron.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chiron-user \
-    lineage_chiron-userdebug \
-    lineage_chiron-eng
+    tequila_chiron-user \
+    tequila_chiron-userdebug \
+    tequila_chiron-eng
